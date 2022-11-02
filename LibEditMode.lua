f7e1f92754a74e2b766fcec989cc26e21c59238b
plusmouse <plusmouse@protonmail.com>
2022-11-02 09:51:59 +0000

    Update with layout management functions

--- LibEditMode.lua
+++ LibEditMode.lua
@@ -5,6 +5,8 @@ local lib = LibStub:NewLibrary("LibEditMode-1-0", 1)
 local frame = CreateFrame("Frame")
 
 local FRAME_ERROR = "This frame isn't used by edit mode"
+local LOAD_ERROR = "You need to call LibEditMode:LoadLayouts first"
+local EDIT_ERROR = "Active layout is not editable"
 
 local layoutInfo
 
@@ -26,12 +28,35 @@ local function GetSystemByFrame(frame)
   return GetSystemByID(systemID, systemIndex)
 end
 
+local function GetLayoutIndex(layoutName)
+  for index, layout in ipairs(layoutInfo.layouts) do
+    if layout.layoutName == layoutName then
+      return index
+    end
+  end
+end
+
+local function GetHighestIndex()
+  local highestLayoutIndexByType = {};
+  for index, layoutInfo in ipairs(layoutInfo.layouts) do
+    if not highestLayoutIndexByType[layoutInfo.layoutType] or highestLayoutIndexByType[layoutInfo.layoutType] < index then
+      highestLayoutIndexByType[layoutInfo.layoutType] = index;
+    end
+  end
+  return highestLayoutIndexByType
+end
+
 function lib:SetGlobalSetting(setting, value)
   C_EditMode.SetAccountSetting(setting, value)
 end
 
+function lib:HasEditModeSettings(frame)
+  return GetSystemByFrame(frame) ~= nil
+end
+
 -- Set an option found in the Enum.EditMode enumerations
 function lib:SetFrameSetting(frame, setting, value)
+  assert(lib:CanEditActiveLayout(), EDIT_ERROR)
   local system = GetSystemByFrame(frame)
 
   assert(system, FRAME_ERROR)
@@ -46,6 +71,7 @@ function lib:SetFrameSetting(frame, setting, value)
 end
 
 function lib:ReanchorFrame(frame, ...)
+  assert(lib:CanEditActiveLayout(), EDIT_ERROR)
   local system = GetSystemByFrame(frame)
 
   assert(system, FRAME_ERROR)
@@ -71,10 +97,101 @@ function lib:LoadLayouts()
   layoutInfo.layouts = tmp
 end
 
+function lib:SaveOnly()
+  assert(layoutInfo, LOAD_ERROR)
+  C_EditMode.SaveLayouts(layoutInfo)
+end
+
 function lib:ApplyChanges()
   assert(not InCombatLockdown(), "Cannot move frames in combat")
-  C_EditMode.SaveLayouts(layoutInfo)
+  lib:SaveOnly()
 
   ShowUIPanel(EditModeManagerFrame)
   HideUIPanel(EditModeManagerFrame)
+end
+
+function lib:DoesLayoutExist(layoutName)
+  assert(layoutInfo, LOAD_ERROR)
+  return GetLayoutIndex(layoutName) ~= nil
+end
+
+function lib:AddLayout(layoutType, layoutName)
+  assert(layoutInfo, LOAD_ERROR)
+  assert(layoutName and layoutName ~= "", "Non-empty string required")
+  assert(not lib:DoesLayoutExist(layoutName), "Layout should not already exist")
+
+  local newLayout = CopyTable(layoutInfo.layouts[1]) -- Modern layout
+
+  newLayout.layoutType = layoutType
+  newLayout.layoutName = layoutName
+
+  local highestLayoutIndexByType = GetHighestIndex()
+
+  local newLayoutIndex;
+  if highestLayoutIndexByType[layoutType] then
+    newLayoutIndex = highestLayoutIndexByType[layoutType] + 1;
+  elseif (layoutType == Enum.EditModeLayoutType.Character) and highestLayoutIndexByType[Enum.EditModeLayoutType.Account] then
+    newLayoutIndex = highestLayoutIndexByType[Enum.EditModeLayoutType.Account] + 1;
+  else
+    newLayoutIndex = Enum.EditModePresetLayoutsMeta.NumValues + 1;
+  end
+
+  table.insert(layoutInfo.layouts, newLayoutIndex, newLayout)
+  C_EditMode.OnLayoutAdded(newLayoutIndex)
+  C_EditMode.SetActiveLayout(newLayoutIndex)
+end
+
+function lib:DeleteLayout(layoutName)
+  assert(layoutInfo, LOAD_ERROR)
+  local index = GetLayoutIndex(layoutName)
+  assert(index ~= nil, "Can't delete layout as it doesn't exist")
+
+  assert(layoutInfo.layouts[index].layoutType ~= Enum.EditModeLayoutType.Preset, "Cannot delete preset layouts")
+
+  table.remove(layoutInfo.layouts, index)
+  C_EditMode.OnLayoutDeleted(index)
+end
+
+function lib:GetEditableLayoutNames()
+  assert(layoutInfo, LOAD_ERROR)
+  local names = {}
+  for _, layout in ipairs(layoutInfo.layouts) do
+    if layout.layoutType ~= Enum.EditModeLayoutType.Preset then
+      table.insert(names, layout.layoutName)
+    end
+  end
+
+  return names
+end
+
+function lib:GetPresetLayoutNames()
+  assert(layoutInfo, LOAD_ERROR)
+  local names = {}
+  for _, layout in ipairs(layoutInfo.layouts) do
+    if layout.layoutType == Enum.EditModeLayoutType.Preset then
+      table.insert(names, layout.layoutName)
+    end
+  end
+
+  return names
+end
+
+function lib:CanEditActiveLayout()
+  assert(layoutInfo, LOAD_ERROR)
+  return layoutInfo.layouts[layoutInfo.activeLayout].layoutType ~= Enum.EditModeLayoutType.Preset
+end
+
+function lib:SetActiveLayout(layoutName)
+  assert(layoutInfo, LOAD_ERROR)
+  assert(lib:DoesLayoutExist(layoutName), "Layout must exist")
+
+  local index = GetLayoutIndex(layoutName)
+
+  layoutInfo.activeLayout = index
+  C_EditMode.SetActiveLayout(index)
+end
+
+function lib:GetActiveLayout()
+  assert(layoutInfo, LOAD_ERROR)
+  return layoutInfo.layouts[layoutInfo.activeLayout].layoutName
 end
--- README.md
+++ README.md
@@ -11,22 +11,72 @@ LibEditMode:ApplyChanges()
 ```
 
 # Usage
-## `LibEditMode:LoadLayouts()`
 
-Required to call before doing any frame settings/anchors. Queries and saves from
-the Blizzard API last saved edit mode frame settings and positions.
+## Initialization and saving
+### `LibEditMode:LoadLayouts()`
 
-## `LibEditMode:ReanchorFrame(frame, ...)`
+Required to call before using any other functions in this library. Queries and saves from
+the Blizzard API last saved edit mode frame settings and positions. You **will**
+need to call this again if the edit mode UI updates the layouts.
 
-For a frame that is managed by edit mode change the default anchor to `...`
-(same parameters as a `frame:SetPoint(...)` call)
+### `LibEditMode:AreLayoutsLoaded()`
 
-## `LibEditMode:SetFrameSetting(frame, setting, value)`
+Has `LibEditMode:LoadLayouts()` been called at some point?
 
-Set an edit mode setting on a frame. Use the enumerations found in the Blizzard
-Lua dumps for the setting ids.
+### `LibEditMode:ApplyChanges()`
 
-## `LibEditMode:SetGlobalSetting(setting, value)`
+Save any changes made and apply any layout changes to the frames. Does not work
+during combat.
 
-Set an edit mode account setting. This is just an alias for
+### `LibEditMode:SaveOnly()`
+
+Save any changes made. Works during combat.
+
+## Frame configuration
+
+### `LibEditMode:ReanchorFrame(frame, ...)`
+
+For a frame that is managed by edit mode change the default anchor. Uses the
+same parameters as a `frame:SetPoint(...)` call for the anchor.
+
+### `LibEditMode:SetFrameSetting(frame, setting, value)`
+
+Set an edit mode setting on an edit mode managed frame. Use the enumerations
+found in the Blizzard Lua dumps for the setting parameter.
+
+### `LibEditMode:SetGlobalSetting(setting, value)`
+
+Set an edit mode account-wide setting. This is just an alias for
 `C_EditMode.SetAccountSetting`
+
+### `LibEditMode:HasEditModeSettings(frame)`
+
+Does this frame have edit mode settings available?
+
+## Layout management
+
+### `LibEditMode:AddLayout(layoutName)`
+
+Create a new layout called `layoutName`. This name must not already be in use.
+
+### `LibEditMode:DeleteLayout(layoutName)`
+
+Delete a layout named `layoutName`. This layout must exist.
+
+### `LibEditMode:SetActiveLayout(layoutName)`
+
+Change the active layout to `layoutName`. This name must exist. You need to call
+`LibEditMode:ApplyChanges` or similar for  this to take effect and persist.
+
+
+### `LibEditMode:DoesLayoutExist(layoutName)`
+
+Does a layout with the name `layoutName` exist?
+
+### `LibEditMode:GetPresetLayoutNames(layoutName)`
+
+Returns a table of the names of all preset non-editable layouts.
+
+### `LibEditMode:GetEditableLayoutNames(layoutName)`
+
+Returns a table of the names of all editable layouts.
